39d772cde46671da464d710267143094510b4c5f
TaTanDuc <117703084+TaTanDuc@users.noreply.github.com>
2024-10-14 17:14:55 +0700

    MET MOI QUA TOI OI

--- veterinaryWebServices/src/main/java/com/team12/veterinaryWebServices/api/invoiceAPI.java
+++ veterinaryWebServices/src/main/java/com/team12/veterinaryWebServices/api/invoiceAPI.java
@@ -2,14 +2,12 @@ package com.team12.veterinaryWebServices.api;
 
 import com.team12.veterinaryWebServices.exception.appException;
 import com.team12.veterinaryWebServices.service.invoiceServices;
-import com.team12.veterinaryWebServices.viewmodel.invoiceVM;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
